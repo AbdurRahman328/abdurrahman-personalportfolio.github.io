3e478a289f1d4e632f3199169c302d707ddc2594
AbdurRahman328 <abdurshakul22@gmail.com>
2024-01-01 18:52:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToLinkedn()
+{
+    window.location.href="https://www.linkedin.com/in/abdur-rahman-s-18b65621a";
+}
+
+function getResume(){
+    window.location.href="https://drive.google.com/file/d/1T3LIVMfCupISxbdIY8yb9Kek8hw6Cp8R/view?usp=sharing";
+}
